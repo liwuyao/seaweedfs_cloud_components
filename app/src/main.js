b55f0112851fad24455118a6a087e94a1059ad76
liwuyao <1181232137@qq.com>
2021-07-14 10:34:21 +0800

    fix bug

--- app/src/main.js
+++ app/src/main.js
@@ -12,6 +12,9 @@ import filter from './filter/index.js'
 const url = window.location.href
 if(url.includes('https://cloud.seaweedfs.com/cluster/')){
     let id = url.split('https://cloud.seaweedfs.com/cluster/')[1]
+    if(id.includes('/')){
+        id = id.split('/')[0]
+    }
     localStorage.clusterId = id
 }
 createApp(App).use(Axios).use(router).use(filter).use(ElementPlus).mount('#app')

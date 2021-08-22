545b318e981f490aceb4173e66fd4ccf59087d94
iwuyao <1181232137@qq.com>
2021-08-22 18:59:10 +0800

    update

--- app/src/main.js
+++ app/src/main.js
@@ -6,10 +6,30 @@ import './assets/_menu.css'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import Axios from './http/axios'
+import axios from 'axios'
 import router from './router/index.js'
 import filter from './filter/index.js'
 import Plugin from './plugin/index.js'
+import {login_dialog_state} from './communication'
+axios.interceptors.request.use((config) => {
+    config.headers['Authorization'] = localStorage.token
+    return config
+    }, (error) => {
+    return Promise.reject(error);
+})
+axios.interceptors.response.use((response) => {
+    return response
 
+  }, (error) => {
+    if(error.response.data){
+        if(error.response.data.code === -1){
+            localStorage.removeItem('token')
+            console.log('token error')
+            login_dialog_state.value = true
+        }
+    }
+    return Promise.reject(error);
+  })
 const url = window.location.href
 let pathArr = url.split('/')
 let id = pathArr.filter((item)=>item.length === 12)[0]
---
+++ app/src/reload.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="reload"   v-loading="loading" element-loading-text="Loading!">
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            loading:true
+        }
+    },
+    mounted() {
+        setTimeout(()=>{
+            this.$router.push(localStorage.last_path)
+        },1000)
+    },
+}
+</script>
+
+<style scoped>
+.reload{
+    height: 100%;
+    width: 100%;
+}
+</style>
